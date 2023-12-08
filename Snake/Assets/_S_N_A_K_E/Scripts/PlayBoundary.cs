715f17c560ee986e23faf537257ca37503424e1f
Vlad <volodymyr.tormoz@curest.se>
2023-12-08 23:31:02 +0100

    bugfix wrong cell size

--- Snake/Assets/_S_N_A_K_E/Scripts/PlayBoundary.cs
+++ Snake/Assets/_S_N_A_K_E/Scripts/PlayBoundary.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
 using NaughtyAttributes;
-using Unity.XR.CoreUtils;
 using UnityEngine;
-using UnityEngine.Serialization;
 using Random = UnityEngine.Random;
 
 namespace Gustorvo.Snake
@@ -15,47 +10,48 @@ namespace Gustorvo.Snake
         [SerializeField] private Transform debugCube;
         [SerializeField] bool debug;
         [SerializeField] Transform debugCubeParent;
-        [SerializeField] private float unitSize = 0.1f; // size of 1 body unit (of snake)
-        [SerializeField] int gridSize = 1;
-        [SerializeField] private BoxCollider collider;
-        private Bounds gameBounds => collider.bounds;
-        private int itemsInRow;
-        private Vector3[] positionsInCube;
+
+        // private int itemsInRow;
+        private Vector3[] cellList;
 
         private void Awake()
         {
-            FormACube();
+            CreateCellsWithinBounds();
         }
 
-        private void FormACube()
+        [SerializeField, Range(1, 2)] private float boxSize;
+        [ShowNativeProperty] Bounds gameBounds => new Bounds(Vector3.zero, Vector3.one * boxSize);
+        [ShowNativeProperty] int itemsInRow => Mathf.CeilToInt(gameBounds.size.x / cellSize);
+
+        [SerializeField, Range(0.05f, 1)] private float cellSize = 0.1f;
+
+        private void CreateCellsWithinBounds()
         {
-            itemsInRow = (int)(gridSize / unitSize);
-
-            int itemsInCube = itemsInRow * itemsInRow * itemsInRow;
-            positionsInCube = new Vector3[itemsInCube];
-
-            for (int i = 0; i < itemsInCube; i++)
+            float halfCellSize = cellSize / 2;
+            Vector3 offset = new Vector3(halfCellSize, halfCellSize, halfCellSize);
+            int totalItemsInBoundingBox = itemsInRow * itemsInRow * itemsInRow;
+            cellList = new Vector3[totalItemsInBoundingBox];
+            for (int i = 0; i < totalItemsInBoundingBox; i++)
             {
                 int j = i % itemsInRow;
                 int k = (i / itemsInRow) % itemsInRow;
                 int l = i / (itemsInRow * itemsInRow);
-                Vector3 pos = new Vector3(
-                    gameBounds.min.x + unitSize * j,
-                    gameBounds.min.y + unitSize * k,
-                    gameBounds.min.z + unitSize * l
-                );
 
-                positionsInCube[i] = pos;
+                float x = gameBounds.min.x + cellSize * j;
+                float y = gameBounds.min.y + cellSize * k;
+                float z = gameBounds.min.z + cellSize * l;
+
+                cellList[i] = new Vector3(x, y, z) + offset;
             }
         }
 
         [Button]
         void InstantiateCubes()
         {
-            for (int i = 0; i < positionsInCube.Length; i++)
+            for (int i = 0; i < cellList.Length; i++)
             {
-                Instantiate(debugCube, position: positionsInCube[i], rotation: Quaternion.identity,
-                    parent: debugCubeParent).localScale = Vector3.one * unitSize;
+                Instantiate(debugCube, position: cellList[i], rotation: Quaternion.identity,
+                    parent: debugCubeParent).localScale = Vector3.one * cellSize;
             }
         }
 
@@ -75,7 +71,7 @@ namespace Gustorvo.Snake
         {
             randomPosition = Vector3.zero;
 
-            if (excludePositions.Length >= positionsInCube.Length)
+            if (excludePositions.Length >= cellList.Length)
             {
                 Debug.LogError("Too many positions to exclude");
                 return false;
@@ -85,8 +81,8 @@ namespace Gustorvo.Snake
             do
             {
                 i++;
-                int randomIndex = Random.Range(0, positionsInCube.Length);
-                randomPosition = positionsInCube[randomIndex];
+                int randomIndex = Random.Range(0, cellList.Length);
+                randomPosition = cellList[randomIndex];
             } while (excludePositions.Contains(randomPosition) && i < 100);
 
             if (i >= 100)
@@ -101,6 +97,13 @@ namespace Gustorvo.Snake
         public bool IsPositionInBounds(Vector3 postion)
         {
             return gameBounds.Contains(postion);
+        }
+
+        private void OnDrawGizmos()
+        {
+            // draw bounds
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireCube(gameBounds.center, gameBounds.size);
         }
     }
 }
